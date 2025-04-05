7975dbfe07dacf9e4dbd9acdcfcc8d83132b72ce
sonoryaann <113286164+sonoryaann@users.noreply.github.com>
2025-04-05 12:26:51 +0200

    Update supabaseClient.js

--- client/src/lib/supabaseClient.js
+++ client/src/lib/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,  // Usa process.env per le variabili d'ambiente in CRA
-  process.env.REACT_APP_SUPABASE_KEY
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
 );
 
 export default supabase;
