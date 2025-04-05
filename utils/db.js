0346e37a5c764e9c849bcc4546193da2fd30f2dc
sonoryaann <113286164+sonoryaann@users.noreply.github.com>
2025-04-05 12:32:47 +0200

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.REACT_APP_SUPABASE_URL, // URL di Supabase
+  process.env.REACT_APP_SUPABASE_ANON_KEY // La chiave anonima
 );
 
 export default supabase;
