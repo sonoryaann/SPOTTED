ddbb4f7295a1cb8c728d70dd77c980bc146e24ab
sonoryaann <113286164+sonoryaann@users.noreply.github.com>
2025-04-06 23:12:49 +0200

    Update supabaseClient.js

--- client/src/lib/supabaseClient.js
+++ client/src/lib/supabaseClient.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
+console.log("Supabase URL:", process.env.REACT_APP_SUPABASE_URL);
+console.log("Supabase Key:", process.env.REACT_APP_SUPABASE_ANON_KEY);
 
 const supabase = createClient(
   process.env.REACT_APP_SUPABASE_URL,
